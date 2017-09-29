d0c78bd57ce3b691583578873038e900cc297e69
Nicolas Berbiche <nic.berbiche@gmail.com>
2017-09-29 16:17:20 -0400

    Refactored the codebase

---
+++ src/js/products/index.js
@@ -0,0 +1,19 @@
+// const generate_cell_row = (accumulator, product) =>
+// `${accumulator}
+// <tr>
+//     <td>${product.code}</td>
+//     <td>${product.name}</td>
+//     <td>${product.inventoryActual}</td>
+//     <td>${product.price}</td>
+//     <td>${product.description || 'N/A'}</td>
+// </tr>`;
+
+
+// fetchJSON(url).then(({ data }) => {
+//     const tableContent = data.reduce(generate_cell_row, '');
+
+//     $('#products')
+//         .html(tableContent)
+//         .parent()
+//         .removeClass('hidden');
+// });
