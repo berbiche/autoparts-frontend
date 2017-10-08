e04f965c10c77b74e99e02ec676825d8c6c65bcf
berbiche <nic.berbiche@gmail.com>
2017-10-07 22:45:08 -0400

    [WIP] Added products/create

---
+++ src/js/products/common.js
@@ -0,0 +1,4 @@
+import Api from '../api';
+
+export const API_PRODUCTS = new Api('products');
+export { $, $$ } from '../utils';
--- src/js/products/create.js
+++ src/js/products/create.js
@@ -1,4 +1,16 @@
-import Api from '../api';
+import { $, $$, API_PRODUCTS } from './common';
 
-const api = new Api('products');
 
+// const form = $('create-form');
+const form = document.getElementsByTagName('form')[0];
+
+
+$('retour').addEventListener('click', () => history.go(-1));
+$('ajouter').addEventListener('click', () => {
+    API_PRODUCTS
+        .create(new FormData(form))
+        .then(res => res.text())
+        .then((data) => {
+            console.log(data);
+        });
+});
